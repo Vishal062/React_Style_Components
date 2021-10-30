a08a4bf7659172287ec5cb381497fb40e009b612
Vishal062 <vs9425348@gmail.com>
2021-10-30 11:27:30 +0530

    Navbar function completed

---
+++ navbarfunction/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./styles.css";
+// import styled from "styled-components";
+import NavBar from "./Components/navbar";
+
+// const Wrapper = styled.div`
+//   padding: 2rem;
+//   border: 1px solid;
+//   border-color: red;
+
+//   h1 {
+//     color: blue;
+//   }
+//   h2 {
+//     color: red;
+
+//     @media all and (max-width: 620px) {
+//       color: green;
+//     }
+//   }
+// `;
+
+// const A = styled.a`
+//   text-decoration: none;
+//   font-size: 24px;
+//   color: ${(props) => (props.color === "yellow" ? "yellow" : "aqua")};
+// `;
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
